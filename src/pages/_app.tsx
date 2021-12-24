fe707a8c3d522bf4527bf3cb1cb48345a1571763
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2021-12-24 20:22:10 +0100

    feat: add design system to tailwind config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,21 @@
-import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import Head from "next/head"
+import "../styles/globals.css"
 
 function App({ Component, pageProps }: AppProps) {
     return (
         <>
             <Head>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                <title>Next Starter Lite</title>
+                <meta lang="en" />
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap"
+                    rel="stylesheet"
+                />
+                <title>Frontend Mentor | GitHub user search app</title>
             </Head>
             <div className="subpixel-antialiased optimize-legibility">
                 <Component {...pageProps} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,40 @@ module.exports = {
     content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
     darkMode: "media", // or false or 'class'
     theme: {
+        colors: {
+            "pure-blue": "#0079FF",
+            "slate-gray": "#697c9a",
+            "ucla-blue": "#4B6A9B",
+            charcoal: "#2B3442",
+            "ghost-white": "#F6F8FF",
+            "splashed-white": "#FEFEFE",
+            white: "#FFFFFF",
+            "dark-jungle-green": "#141D2F",
+            "medium-jungle-green": "#1E2A47",
+        },
+        fontFamily: {
+            sans: "'Space Mono', sans-serif",
+            body: "'Space Mono', sans-serif",
+            heading: "'Space Mono', sans-serif",
+        },
+        fontSize: {
+            body: ["15px", "25px"],
+            h1: ["26px", "38px"],
+            h2: ["22px", "33px"],
+            h3: ["16px", "24px"],
+            h4: ["13px", "20px"],
+        },
+        fontWeight: {
+            body: 400,
+            h1: 700,
+            h2: 700,
+            h3: 400,
+            h4: 400,
+        },
         extend: {},
     },
     variants: {
         extend: {},
-    },
-    corePlugins: {
-        container: false,
     },
     plugins: [
         plugin(function ({ addUtilities }) {
